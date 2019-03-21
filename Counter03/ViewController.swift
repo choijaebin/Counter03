8548436775e41089e1c3ecd99c04be395b894544
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-03-21 12:33:42 +0900

    count

--- Counter03/ViewController.swift
+++ Counter03/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
+    var count = 0
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lbl.text = "0"
     }
 
+    @IBAction func btn(_ sender: Any) {
+        lbl.text = String(count)
+        if count != 9 {
+            count += 1
+            print(count)
+            lbl.text = String(count)
+        }else{
+            count = 0;
+            lbl.text = String(count)
+        }
 
+    }
 }
 
